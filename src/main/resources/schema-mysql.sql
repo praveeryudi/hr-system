f6be9a62d1575a3db19e9daa9e5f50230ec01295
Praveer Das <praveer.das@dish.com>
2020-06-01 20:12:02 +0530

    new feature - maintenance

--- src/main/resources/schema-mysql.sql
+++ src/main/resources/schema-mysql.sql
@@ -56,6 +56,7 @@ INSERT INTO employee(empId,empName)values('emp003','emp3');*/
 /*  SRIVEN MAINTENANCE DB TABLES */
 DROP TABLE IF EXISTS maintenance_txn;
 DROP TABLE IF EXISTS flat_maintenance_lookup;
+commit;
 
 CREATE TABLE IF NOT EXISTS flat_maintenance_lookup (
   flat_number VARCHAR(10) NOT NULL,
@@ -80,8 +81,78 @@ INSERT INTO flat_maintenance_lookup values('014','Sohel Abdulajij Momin', 4300);
 INSERT INTO flat_maintenance_lookup values('015','Shekhar', 4150);
 INSERT INTO flat_maintenance_lookup values('016','Debashish Behera', 4300);
 
+-- 1st Floor residents
+INSERT INTO flat_maintenance_lookup values('101','Nilesh More', 4300);
+INSERT INTO flat_maintenance_lookup values('102','Pradeepta Swain', 4800);
+INSERT INTO flat_maintenance_lookup values('103','Kaushik', 4400);
+INSERT INTO flat_maintenance_lookup values('104','Nirmal Paramban', 4800);
+INSERT INTO flat_maintenance_lookup values('105','Sonit Patel', 4300);
+INSERT INTO flat_maintenance_lookup values('106','Dhanendra Jain', 4300);
+INSERT INTO flat_maintenance_lookup values('107','Swathi', 4300);
+INSERT INTO flat_maintenance_lookup values('108','Deepak Kumar', 4300);
+INSERT INTO flat_maintenance_lookup values('109','Aakash Gulati', 4300);
+INSERT INTO flat_maintenance_lookup values('110','Rohit Chakraborty', 4800);
+INSERT INTO flat_maintenance_lookup values('111','Ambuj', 4400);
+INSERT INTO flat_maintenance_lookup values('112','Ashish Jena', 4800);
+INSERT INTO flat_maintenance_lookup values('113','Indraneel Baul', 4300);
+INSERT INTO flat_maintenance_lookup values('114','Mahesh Gopalakrishnan', 4300);
+INSERT INTO flat_maintenance_lookup values('115','Ajith M', 4300);
+INSERT INTO flat_maintenance_lookup values('116','Deepak', 4300);
+
 -- 2nd Floor residents
+INSERT INTO flat_maintenance_lookup values('201','Anurag Hathimare', 4300);
+INSERT INTO flat_maintenance_lookup values('202','Hardik', 4800);
+INSERT INTO flat_maintenance_lookup values('203','Smita Pattanaik', 4400);
+INSERT INTO flat_maintenance_lookup values('204','Dhivya Arumugam', 4800);
+INSERT INTO flat_maintenance_lookup values('205','Nabil N Sayyad', 4300);
+INSERT INTO flat_maintenance_lookup values('206','Subhranshu', 4300);
+INSERT INTO flat_maintenance_lookup values('207','Rini Namratha', 4300);
+INSERT INTO flat_maintenance_lookup values('208','Dhinesh Kumar', 4300);
+INSERT INTO flat_maintenance_lookup values('209','Amjad Ali Khan Pathan', 4300);
+INSERT INTO flat_maintenance_lookup values('210','Sonima Dev', 4800);
+INSERT INTO flat_maintenance_lookup values('211','Harish', 4400);
+INSERT INTO flat_maintenance_lookup values('212','Lalatendu Das', 4800);
 INSERT INTO flat_maintenance_lookup values('213','Jeet Kaushik', 4300);
+INSERT INTO flat_maintenance_lookup values('214','Gurusiddesh Kudiker', 4300);
+INSERT INTO flat_maintenance_lookup values('215','Himanshu Gupta', 4300);
+INSERT INTO flat_maintenance_lookup values('216','Gopichand', 4300);
+
+-- 3rd Floor residents
+INSERT INTO flat_maintenance_lookup values('301','Chetan Yamger', 4300);
+INSERT INTO flat_maintenance_lookup values('302','Piyush Jain', 4800);
+INSERT INTO flat_maintenance_lookup values('303','Sujit', 4400);
+INSERT INTO flat_maintenance_lookup values('304','Layeequr Rahman', 4800);
+INSERT INTO flat_maintenance_lookup values('305','Rahul Raina', 4300);
+INSERT INTO flat_maintenance_lookup values('306','Dhaval', 4300);
+INSERT INTO flat_maintenance_lookup values('307','Praveer', 4300);
+INSERT INTO flat_maintenance_lookup values('308','Dhruthi', 4300);
+INSERT INTO flat_maintenance_lookup values('309','Tarun Bhandary', 4300);
+INSERT INTO flat_maintenance_lookup values('310','Jyotideep', 4800);
+INSERT INTO flat_maintenance_lookup values('311','Prabhat', 4400);
+INSERT INTO flat_maintenance_lookup values('312','Vicknesh Ranganath', 4800);
+INSERT INTO flat_maintenance_lookup values('313','PR Gopal', 4300);
+INSERT INTO flat_maintenance_lookup values('314','Sitesh', 4300);
+INSERT INTO flat_maintenance_lookup values('315','Krishan Bhat', 4300);
+INSERT INTO flat_maintenance_lookup values('316','Dipayan Choudhury', 4300);
+
+-- 4th Floor residents
+INSERT INTO flat_maintenance_lookup values('401','Rajkumar', 4300);
+INSERT INTO flat_maintenance_lookup values('402','Ramesh', 4800);
+INSERT INTO flat_maintenance_lookup values('403','Shambu Suman', 4400);
+INSERT INTO flat_maintenance_lookup values('404','Satish Golukonda', 4800);
+INSERT INTO flat_maintenance_lookup values('405','Abhishek Kumar', 4300);
+INSERT INTO flat_maintenance_lookup values('406','Arindam Ghosh', 4300);
+INSERT INTO flat_maintenance_lookup values('407','Yathartha Kulshreshta', 4300);
+INSERT INTO flat_maintenance_lookup values('408','Sunil', 4300);
+INSERT INTO flat_maintenance_lookup values('409','Praveen VV', 4300);
+INSERT INTO flat_maintenance_lookup values('410','Sunil Ganatra', 4800);
+INSERT INTO flat_maintenance_lookup values('411','Abilash EP', 4400);
+INSERT INTO flat_maintenance_lookup values('412','Ajay Dev', 4800);
+INSERT INTO flat_maintenance_lookup values('413','Mayank Gupta', 4300);
+INSERT INTO flat_maintenance_lookup values('414','Vivek Chabbi', 4300);
+INSERT INTO flat_maintenance_lookup values('415','Abhishek Srivastava', 4300);
+INSERT INTO flat_maintenance_lookup values('416','Jatin Bajaj', 4300);
+
 commit;
 
 CREATE TABLE IF NOT EXISTS maintenance_txn (
@@ -96,8 +167,8 @@ CREATE TABLE IF NOT EXISTS maintenance_txn (
   PRIMARY KEY (txn_id)
 );
 
-INSERT INTO maintenance_txn values(101,'007', '2020-05-06', 'MAY', '2020', 4500, 'CASH', 0);
-INSERT INTO maintenance_txn values(102,'213', '2020-05-06', 'MAY', '2020', 4300, 'CASH', 0);
+--INSERT INTO maintenance_txn values(101,'007', '2020-05-06', 'MAY', '2020', 4500, 'CASH', 0);
+--INSERT INTO maintenance_txn values(102,'213', '2020-05-06', 'MAY', '2020', 4300, 'CASH', 0);
 
 commit;
 
