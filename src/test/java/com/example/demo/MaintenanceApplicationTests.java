f6e66dc9e39dc650cebada3907a7bef7d88e64c4
Praveer Das <praveer.das@dish.com>
2020-06-18 00:38:34 +0530

    added H2 db dependency

--- src/test/java/com/example/demo/MaintenanceApplicationTests.java
+++ src/test/java/com/example/demo/MaintenanceApplicationTests.java
@@ -1,17 +1,14 @@
 package com.example.demo;
 
 import com.example.demo.dao.FlatMaintenanceLookUpDAO;
-import com.example.demo.dao.MaintenanceTxnDAO;
 import com.example.demo.entity.FlatMaintenanceLookUp;
-import com.example.demo.entity.MaintenanceTxn;
-import com.example.demo.request.TxnRequest;
 import com.example.demo.service.FlatMaintenanceService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
@@ -24,10 +21,9 @@ import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
-//@RunWith(PowerMockRunner.class)
 @SpringBootTest
 @ActiveProfiles(profiles = "dev")
-//@PrepareForTest({MaintenanceUtil.class})
+@DataJpaTest
 public class MaintenanceApplicationTests {
 
 	private static final Logger log = LoggerFactory.getLogger(MaintenanceApplicationTests.class);
@@ -36,12 +32,6 @@ public class MaintenanceApplicationTests {
 	private FlatMaintenanceService flatMaintenanceService;
 	@MockBean
 	private FlatMaintenanceLookUpDAO flatMaintenanceLookUpDAO;
-	@MockBean
-	private MaintenanceTxnDAO maintenanceTxnDAO;
-	@Mock
-	private TxnRequest txnRequest;
-	@Mock
-	private MaintenanceTxn maintenanceTxn;
 
 	@Test
 	public void testFlatLookup() throws Exception {
@@ -53,23 +43,6 @@ public class MaintenanceApplicationTests {
 				.andDo(print())
 				.andExpect(status().isOk());*/
 	}
-
-	/*@Test
-	public void testAddMaintenance() {
-		log.info("unit testing controller addTransaction()...");
-		txnRequest.setTxnDate(new Date());
-		txnRequest.setSelectedMonth("JUN");
-		txnRequest.setSelectedYear("2020");
-		txnRequest.setExpectedMaintenance(430.0);
-		txnRequest.setFlatNumber("1003");
-		txnRequest.setActualPayment(430.0);
-		txnRequest.setPaymentMode("ONLINE");
-
-		PowerMockito.mockStatic(MaintenanceUtil.class);
-		when(MaintenanceUtil.getPreviousMonthYear("JUN", "2020")).thenReturn(new String[]{"MAY", "2020"});
-		when(maintenanceTxnDAO.getTxn("MAY", "2020", "1003")).thenReturn(null);
-		when(maintenanceTxnDAO.saveAndFlush(maintenanceTxn)).thenReturn(maintenanceTxn);
-	}*/
 
 	private List<FlatMaintenanceLookUp> getAllFlatDataMockReturn() {
 
