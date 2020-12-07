95d95c41215f65f89e239c0aaa3077f288572535
Das, Praveer <praveer.das@dish.com>
2020-12-07 18:14:56 +0530

    Added period analytics

--- src/main/java/com/example/demo/controller/BIController.java
+++ src/main/java/com/example/demo/controller/BIController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import com.example.demo.service.BIService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -21,9 +22,13 @@ public class BIController {
         this.BIService = BIService;
     }
 
+    @ApiOperation(value = "Gets maintenance data for the period selected",
+            notes = "Gets maintenance data for the period selected",
+            response = Map.class,
+            nickname = "getDataForSelectedPeriod")
     @GetMapping(value = "/periodData/{duration}")
     @ResponseStatus(HttpStatus.OK)
-    public Map<String, Double> getAllMonths(@PathVariable(value = "duration") final String duration) {
+    public Map<String, Double> getDataForSelectedPeriod(@PathVariable(value = "duration") final String duration) {
         return BIService.getMonthsList(duration);
     }
 
